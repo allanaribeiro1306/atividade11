77fb7f8d48a8115723fbf10dc92d9f225b4ef0dc
vitorlima1235 <vitorlima150703@gmail.com>
2024-04-30 22:46:13 -0300

    soluçao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TitioComponent } from './titio/titio.component';
+import { ConteudoComponent } from './conteudo/conteudo.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,TitioComponent,ConteudoComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
